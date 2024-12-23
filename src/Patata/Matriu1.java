444035bdb51548a0c65bdcf576fe92761c11bc01
alex24050714 <alexmontserrat2405a@alumnes.ilerna.com>
2024-12-23 13:05:00 +0100

    Update Matriu1.java

--- src/Patata/Matriu1.java
+++ src/Patata/Matriu1.java
@@ -1,10 +1,32 @@
 package Patata;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class Matriu1 {
 
+	static Scanner scan = new Scanner(System.in);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Random rand = new Random();
+		
+			int [][] matriu = new int [5][5];
+		
+			int aleatorio = 100;
+		
+		System.out.println("Que numero quieres que encuentre?: ");
+			int patata = scan.nextInt();
+		
+			for (int i = 0; i < matriu.length; i++) {
+				for(int j = 0; j < matriu[0].length; j++) {
+					matriu[i][j]=rand.nextInt(aleatorio) + 1;
+			}
+		}
+		for (int i = 0; i < matriu.length; i++) {
+			for(int j = 0; j < matriu[i].length; j++) {
+				System.out.print(matriu[i][j] + "  ");
+			}
+			System.out.println();
+		}
 	}
 
 }
